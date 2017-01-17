02c6dd8af50351fc2359eecc72132119236de02d
adrianke77 <adrianke77@gmail.com>
2017-01-17 18:00:25 +0800

    fixed selection sort, was doing some other kind of sort previously

--- selection_sort.js
+++ selection_sort.js
@@ -1,11 +1,17 @@
 function SortedArray( array ) {
   let numStore = 0;
-  for ( let i = 1; i < array.length; i++ ) //i is end of current segment to sort
-    for ( let j = 0; j < i; j++ ) { // j iterates through current segment to sort
-    if ( array[ i ] < array[ j ] ) { // if end of segment less than iterated value in segment
-      numStore = array[ j ]; // then swap values
-      array[ j ] = array[ i ];
-      array[ i ] = numStore;
+  for ( i = 0; i < array.length; i++ ) { //tracks start of the unsorted length
+    let smallestInUnsorted = Infinity;
+    let smallestInUnsortedPosition = 0;
+    for ( j = i; j < array.length; j++ ) //iterates through unsorted part
+      if ( array[ j ] < smallestInUnsorted ) { // find smallest value in unsorted part
+        smallestInUnsorted = array[ j ]
+        smallestInUnsortedPosition = j
+      }
+    if ( smallestInUnsorted < array[ i ] ) { // swap only if smallest value found is less than end of sorted part 
+      numStore = array[ i ];
+      array[ i ] = array[ smallestInUnsortedPosition ];
+      array[ smallestInUnsortedPosition ] = numStore;
     }
   }
   this.array = array;
@@ -13,12 +19,8 @@ function SortedArray( array ) {
 
 console.time( "sorttime" )
 sortedArray =
-  new SortedArray( [ 213, 123, 6, 6134, 134, 135, 6, 13413, 447, 468, 46, 23,
-    2, 234, 234, 24, 34, 236, 134, 134, 14, 7, 2458356835, 24, 6, 4613, 4, 5,
-    35, 34, 624, 72, 472, 47, 246, 2, 64, 34, 634, 2458, 4568, 5679, 46,
-    2456, 3, 4613, 5413, 54, 1346, 23, 6245, 34, 34, 13, 513, 5, 134, 143, 24,
-    55, 47, 6, 342, 234, 634, 6, 43, 24, 6245, 724, 7, 4257, 24, 634, 134, 5,
-    13, 4513, 5, 1345, 134, 51, 436, 1346, 134, 6134, 6, 57, 3568, 46, 4, 6 ] )
+  new SortedArray( [ 1, 5, 4, 2, 3, 6, 9, 7, 8, 23, 245, 3457, 8, 24, 2, 68, 3,
+    24, 24, 45, 3569, 3569356, 7, 3254, 134, 9 ] )
 console.timeEnd( "sorttime" )
 
 console.log( "sorted array is ", sortedArray.array )
